db25e263bae43095bf8325f0ac227b9c6b2f80b7
Sam <samuelrush@gmail.com>
2019-10-24 02:15:49 +0000

    finished the edge cases

--- fetcher.js
+++ fetcher.js
@@ -4,24 +4,27 @@ const args = process.argv.slice(2);
 
 if (!fs.existsSync(args[1])) {
   console.log("Local file path given is invalid")
-  throw err
+  return
 } else {
   request(args[0], function(error, response, body) {
     if (error !== null){
       console.log("The error value was not null, meaning the domain was not found!")
-      return
-    }
+    } else if (response.statusCode !== 200){
+      console.log("The response code is not 200 meaning the URL has an error!")
+    } else {
     //console.log('statusCode:', response && response.statusCode);
     fs.writeFile(args[1], body, function (err) {
       if (err) {throw err;}
     })
+    }
   })
-
-  .on('response', function(response) {
+  .on('response', function(responses) {
     // unmodified http.IncomingMessage object
-    response.on('data', function(data) {
-      // compressed data as it is received
-      console.log("Downloaded and saved " + " " + data.length + " bytes to " + args[1])
+    responses.on('data', function(data) {
+      if (responses.statusCode === 200) {
+        // compressed data as it is received
+        console.log("Downloaded and saved " + " " + data.length + " bytes to " + args[1])
+      }
     })
   });
 }
