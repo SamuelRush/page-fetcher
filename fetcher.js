35491eb12cb29e8505e610de12dfbbd5240fff80
Sam <samuelrush@gmail.com>
2019-10-24 01:19:11 +0000

    Accounted for 2/3 errors

--- fetcher.js
+++ fetcher.js
@@ -2,23 +2,26 @@ const request = require('request');
 const fs = require('fs');
 const args = process.argv.slice(2);
 
-request(args[0], function(error, response, body) {
-  //console.log('error', error);
-  //console.log('statusCode:', response && response.statusCode);
-  //console.log('body:', body);
-
-
-
-  fs.writeFile(args[1], body, function (err) {
-    if (err) {throw err;}
+if (!fs.existsSync(args[1])) {
+  console.log("Local file path given is invalid")
+  throw err
+} else {
+  request(args[0], function(error, response, body) {
+    if (error !== null){
+      console.log("The error value was not null, meaning the domain was not found!")
+      return
+    }
+    //console.log('statusCode:', response && response.statusCode);
+    fs.writeFile(args[1], body, function (err) {
+      if (err) {throw err;}
+    })
   })
-})
-.on('response', function(response) {
-  // unmodified http.IncomingMessage object
-  response.on('data', function(data) {
-    // compressed data as it is received
-    console.log("Downloaded and saved " + " " + data.length + " bytes to " + args[1])
-  })
-});
 
-
+  .on('response', function(response) {
+    // unmodified http.IncomingMessage object
+    response.on('data', function(data) {
+      // compressed data as it is received
+      console.log("Downloaded and saved " + " " + data.length + " bytes to " + args[1])
+    })
+  });
+}
